b6955d0e65ee4881ba238e995303977bfb8b23c0
ParmanGitijah <Parman.gitijah@chasacademy.se>
2025-09-29 16:11:44 +0200

    Closes #49 - Har nu skapat första databasen enligt ER-diagrammet. Testat och köra programmet och se databasen och det var rätt!

---
+++ Models/Cars.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PegasusBackend.Models
+{
+    public class Cars
+    {
+        [Key]
+        public int CarId { get; set; }
+
+        [Required]
+        [MaxLength(20)]
+        public string Make { get; set; } = string.Empty;
+
+        [Required]
+        [MaxLength(20)]
+        public string Model { get; set; } = string.Empty;
+
+        [MaxLength(20)]
+        public string? Type { get; set; }
+
+        [Range(1, 8)] 
+        public int Capacity { get; set; }
+
+        [Required]
+        [MaxLength(10)]
+        public string LicensePlate { get; set; } = string.Empty;
+
+        public virtual Drivers? Driver { get; set; }
+    }
+}

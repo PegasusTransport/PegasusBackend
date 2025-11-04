94bb4bcdd4485321ef7792cb395087f60424e03a
Joel <joel.300@live.se>
2025-11-04 14:40:57 +0100

    fixed ServiceExtension

--- Configurations/ServiceExtentions.cs
+++ Configurations/ServiceExtentions.cs
@@ -6,7 +6,8 @@ using Microsoft.OpenApi.Models;
 using PegasusBackend.Data;
 using PegasusBackend.Filters;
 using PegasusBackend.Models;
-using PegasusBackend.Services.BackgroundServices;
+using PegasusBackend.Services.Implementations;
+using PegasusBackend.Helpers;
 
 
 namespace PegasusBackend.Configurations
